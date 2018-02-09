dc60f54dd5ae28edc570b77747c6744a65a01a15
Mehul Jain <mr.mehul.jain@gmail.com>
2018-02-08 20:59:43 -0500

    Update bookRouter.js

--- src/routes/bookRouter.js
+++ src/routes/bookRouter.js
@@ -5,8 +5,8 @@ var passport = require('passport');
 //https://cloud.mongodb.com/
 //test
 //testmongo@test.com
-//newpassword.123
-const url = 'mongodb://test:newpassword.123@cluster0-shard-00-00-jrkyu.mongodb.net:27017,cluster0-shard-00-01-jrkyu.mongodb.net:27017,cluster0-shard-00-02-jrkyu.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin';
+
+
 
 var bookRouter = express.Router();
 bookRouter.use(function(request, response, next){
