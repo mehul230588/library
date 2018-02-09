1d277e93e7be052ce5e0306a2648df2d5c3e9557
Mehul Jain <mr.mehul.jain@gmail.com>
2018-02-08 21:00:50 -0500

    Update adminRouter.js

--- src/routes/adminRouter.js
+++ src/routes/adminRouter.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var MongoClient = require('mongodb').MongoClient;
-const url = 'mongodb://test:newpassword.123@cluster0-shard-00-00-jrkyu.mongodb.net:27017,cluster0-shard-00-01-jrkyu.mongodb.net:27017,cluster0-shard-00-02-jrkyu.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin';
+
 
 var adminRouter = express.Router();
 // Connection URL
