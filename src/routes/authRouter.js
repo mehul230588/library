b21d5a38a57d0a93fca367e7569b48c93ab1891b
Mehul Jain <mr.mehul.jain@gmail.com>
2018-02-08 21:00:22 -0500

    Update authRouter.js

--- src/routes/authRouter.js
+++ src/routes/authRouter.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var MongoClient = require('mongodb').MongoClient;
 var passport = require('passport');
-const url = 'mongodb://test:newpassword.123@cluster0-shard-00-00-jrkyu.mongodb.net:27017,cluster0-shard-00-01-jrkyu.mongodb.net:27017,cluster0-shard-00-02-jrkyu.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin';
 
 var authRouter = express.Router();
 
